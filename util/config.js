61cc7a03bc437942d94893f86dda242e1892d32e
Joonas Rantala <joonasrantala@hotmail.com>
2026-08-05 14:22:21 +0300

    not working

--- util/config.js
+++ util/config.js
@@ -1,7 +1,11 @@
 require('dotenv').config()
 
+const DATABASE_URL = process.env.TESTING
+  ? process.env.TEST_DATABASE_URL
+  : process.env.DATABASE_URL
+
 module.exports = {
-  SECRET: "SECRET",
-  DATABASE_URL: process.env.DATABASE_URL,
+  SECRET: process.env.SECRET,
+  DATABASE_URL,
   PORT: process.env.PORT || 3001,
 }
